7f0bc98c8fbf4ee849a675a746af82854f8ea7f0
Bogdoslav <bogdoslavik@gmail.com>
2025-10-23 17:53:27 +0700

    feat: add outputFileTracingRoot to next.js config

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    typedRoutes: true
+    typedRoutes: true,
+    outputFileTracingRoot: __dirname
   },
   transpilePackages: [
     "@rainbow-me/rainbowkit",
