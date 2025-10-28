88e440d51654c3406b1f5899902314809d4d62f9
Bogdoslav <bogdoslavik@gmail.com>
2025-10-28 15:27:24 +0700

    refactor: update and expand payment methods for US and TH

--- app/payment-methods.ts
+++ app/payment-methods.ts
@@ -7,8 +7,28 @@ const EUR_METHODS: string[] = [
   "Wise",
 ];
 
+// Only P2P methods - when citizen can transfer to another citizen
 export const PAYMENT_METHODS: PaymentMethodsByCountry = {
   // TODO complete for US, TH and other countries
-  US: ["Fedwire", "ACH", "Zelle", "Venmo"],
-  TH: ["Bangkok Bank", "Kasikorn", "PromptPay"],
+  US: [
+    "Zelle",
+    "Venmo",
+    "Cash App",
+    "PayPal",
+    "Apple Cash",
+    "Google Pay Balance",
+    "Chime Pay Friends",
+    "Facebook Pay",
+  ],
+  TH: [
+    "Bangkok Bank",
+    "Kasikorn Bank",
+    "Siam Commercial Bank (SCB)",
+    "Krungthai Bank",
+    "Krungsri (Bank of Ayudhya)",
+    "TMBThanachart (TTB)",
+    "CIMB Thai",
+    "PromptPay",
+    "TrueMoney Wallet",
+  ],
 }
