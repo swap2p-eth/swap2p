ffe53fa43089f62d77cf1fc804c96bed792d6c50
Bogdoslav <bogdoslavik@gmail.com>
2025-10-28 16:31:00 +0700

    refactor: increase optimizer runs in hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -29,7 +29,7 @@ const config: HardhatUserConfig = {
           viaIR: false,
           optimizer: {
             enabled: true,
-            runs: 1,
+            runs: 100000,
           },
         },
       },
